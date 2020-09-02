20e8d20152a5f6081d40cb59a2651a333b97a2c1
zhangchao <18910146715@163.com>
2020-09-03 00:35:17 +0800

    Full GC频繁发生之:屌丝版s区内存分配

--- src/main/java/com/zc/jvmtuning/gc/Demo7.java
+++ src/main/java/com/zc/jvmtuning/gc/Demo7.java
@@ -12,10 +12,13 @@ package com.zc.jvmtuning.gc;
  *  优化:调大年轻代内存,增加s区内存,让s区有足够空间存放没次y gc的存活内存
  *
  *  优化后的jvm参数:
- *  step1:单纯加大新生代内存
+ *
+ *  step1:
+ *  单纯加大新生代内存
  *  -XX:NewSize=209715200 -XX:MaxNewSize=209715200 -XX:InitialHeapSize=314572800 -XX:MaxHeapSize=314572800 -XX:SurvivorRatio=8 -XX:MaxTenuringThreshold=15 -XX:PretenureSizeThreshold=20971520 -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -Xloggc:gc.log
  *  结果:发现老年代发生频率有所降低,但是仍然不够
- *
+ *  原因分析:
+ *  仅升级新生代内存->s区内存20M 每次e区存活10M放入
  *  step2
  *  调整e区s区内存比例(注意:一定要权衡新生代内存生产速度,比例太低,会导致y gc频繁)
  *
@@ -25,6 +28,12 @@ package com.zc.jvmtuning.gc;
  *
  *  step3 :如果实际情况,服务器无法拓展内存呢?
  *  -XX:NewSize=104857600 -XX:MaxNewSize=104857600 -XX:InitialHeapSize=209715200 -XX:MaxHeapSize=209715200 -XX:SurvivorRatio=2 -XX:MaxTenuringThreshold=15 -XX:PretenureSizeThreshold=20971520 -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -Xloggc:gc.log
+ *
+ *  事实上这里存在一个隐患 如果把loadData后面创建最后四个的代码提到循环前面,结果可能就不这么乐观了(见demo7_2.jpg)
+ *
+ *  原因分析:新生代s区内存(25M)小于一次方法执行可能存活的最大内存(40M)
+ *
+ *  实际生产中多线程可能更复杂,因此该情况首要是新增新生代内存大小,提升s区内存占比,使其尽可能总能存储每次y gc的存活对象
  * @Author: zhangchao
  **/
 public class Demo7 {
@@ -36,15 +45,17 @@ public class Demo7 {
     }
 
     private static void loadData() throws InterruptedException {
+        byte[] data1 = new byte[10 * 1024 * 1024];
+        byte[] data2 = new byte[10 * 1024 * 1024];
+        byte[] data3 = new byte[10 * 1024 * 1024];
+        data3 = new  byte[10 * 1024 * 1024];
+
         byte[] data = null;
         for (int j=1;j<=4 ;j++) {
             data = new byte[10 * 1024 * 1024];
         }
         data = null;
-        byte[] data1 = new byte[10 * 1024 * 1024];
-        byte[] data2 = new byte[10 * 1024 * 1024];
-        byte[] data3 = new byte[10 * 1024 * 1024];
-        data3 = new  byte[10 * 1024 * 1024];
+
 
         Thread.sleep(1000);
     }
